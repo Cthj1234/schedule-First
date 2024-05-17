54b8a913cea5e5860fce87f95c3fcfda515d3d0a
hanhojin <ghwls8172@naver.com>
2024-05-17 21:50:45 +0900

    요구사항 4,5 기능구현 + Postman 테스트 완료

--- src/main/java/com/sparta/schedulefirst/controller/ScheduleController.java
+++ src/main/java/com/sparta/schedulefirst/controller/ScheduleController.java
@@ -22,25 +22,28 @@ public class ScheduleController {
         Schedule schedule = new Schedule(requestDto);
         int maxId = !schedule_List.isEmpty() ? schedule_List.size() + 1 : 1;
         schedule.setId(maxId);
-        schedule_List.put(maxId,schedule);
+        schedule_List.put(maxId, schedule);
 
         return new ScheduleResponseDto(schedule);
     }
-    
+
     // 요구사항2 선택 한 일정 조회
     @GetMapping("/find/{id}")
-    public ScheduleResponseDto findSchedule(@PathVariable int id){
+    public ScheduleResponseDto findSchedule(@PathVariable int id) {
         if (schedule_List.containsKey(id)) {
             return new ScheduleResponseDto(schedule_List.get(id));
-        }else{
+        } else {
             throw new IllegalArgumentException(id + "번의 일정은 저장 되어있지 않습니다.");
         }
     }
-    
+
     // 요구사항3 등록된 일정 전체 조회 (단 등록일 순으로 내림차순)
     @GetMapping("/findAll")
-    public List<ScheduleResponseDto> findAll(){
+    public List<ScheduleResponseDto> findAll() {
 
+        if (schedule_List.isEmpty()) {
+            throw new IllegalArgumentException("등록된 일정이 없습니다.");
+        }
         //내림차순 정렬 (ResponseDto 내부에서 compareTo)
         // Map To List
         List<ScheduleResponseDto> responseList = new ArrayList<>(schedule_List.values().stream()
@@ -50,6 +53,45 @@ public class ScheduleController {
         return responseList;
     }
 
+    // 요구사항4
+    //1. 선택한 일정의 `할일 제목`, `할일 내용`, `담당자`을 수정할 수 있습니다.
+    //1. 서버에 일정 수정을 요청할 때 `비밀번호`를 함께 전달합니다.
+    //2. 수정된 일정의 정보를 반환 받아 확인할 수 있습니다.
+    @PostMapping("/update/{id}")
+    public int update(@PathVariable int id, @RequestBody ScheduleRequestDto requestDto) {
+        if (!schedule_List.containsKey(id)) {
+            throw new IllegalArgumentException("해당 id는 존재하지 않습니다.");
+        }
 
+        Schedule schedule = schedule_List.get(id);
 
+        // 비밀번호가 올바르지 않는 경우 예외처리
+        if (!Objects.equals(schedule.getPassword(), requestDto.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        // 에러 없는 경우 수정
+        schedule.update(requestDto);
+
+        schedule_List.put(id, schedule);
+        return id;
+    }
+
+    //요구사항5 password를 전달받아 삭제
+    @DeleteMapping("/delete/{id}")
+    public int delete(@PathVariable int id, @RequestBody String password) {
+        if (!schedule_List.containsKey(id)) {
+            throw new IllegalArgumentException("해당 id는 존재하지 않습니다.");
+        }
+
+        Schedule schedule = schedule_List.get(id);
+
+        // 비밀번호가 올바르지 않는 경우 예외처리
+        if (!Objects.equals(schedule.getPassword(), password)) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        schedule_List.remove(id);
+        return id;
+    }
 }
--- src/main/java/com/sparta/schedulefirst/entity/Schedule.java
+++ src/main/java/com/sparta/schedulefirst/entity/Schedule.java
@@ -30,4 +30,11 @@ public class Schedule {
         this.created_Time = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
     }
 
+    // 할일 제목, 할일 내용, 담당자, 변경시간 수정
+    public void update(ScheduleRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.contents = requestDto.getContents();
+        this.manager = requestDto.getManager();
+        this.created_Time = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
+    }
 }

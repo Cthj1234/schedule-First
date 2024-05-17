76375113916042999042e589ad0a136bd7ad87a3
hanhojin <ghwls8172@naver.com>
2024-05-17 13:36:42 +0900

    init test

---
+++ src/main/java/com/sparta/schedulefirst/ScheduleFirstApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.schedulefirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleFirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/schedulefirst/ScheduleFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.schedulefirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

36cd98bab53395fcec4a44569110b60706db6b97
hanhojin <ghwls8172@naver.com>
2024-05-30 17:22:45 +0900

    init test

--- src/main/java/com/sparta/schedulefirst/controller/ScheduleController.java
+++ src/main/java/com/sparta/schedulefirst/controller/ScheduleController.java
@@ -9,7 +9,7 @@ import java.util.*;
 
 // URI를 반환하지 않고 Postman으로 테스트하므로 RestController 사용
 @RestController
-@RequestMapping("/schedule")
+@RequestMapping("/api")
 public class ScheduleController {
 
     // Schedule 객체 저장소
@@ -57,7 +57,7 @@ public class ScheduleController {
     //1. 선택한 일정의 `할일 제목`, `할일 내용`, `담당자`을 수정할 수 있습니다.
     //1. 서버에 일정 수정을 요청할 때 `비밀번호`를 함께 전달합니다.
     //2. 수정된 일정의 정보를 반환 받아 확인할 수 있습니다.
-    @PostMapping("/update/{id}")
+    @PutMapping("/update/{id}")
     public int update(@PathVariable int id, @RequestBody ScheduleRequestDto requestDto) {
         if (!schedule_List.containsKey(id)) {
             throw new IllegalArgumentException("해당 id는 존재하지 않습니다.");
